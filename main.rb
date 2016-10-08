a218d01e728ad900856cc6b075ab8aafaa1aaab3
57160172 <chutinan_flimlovetol@hotmail.com>
2016-10-08 15:46:20 +0700

    Update main.rb

--- main.rb
+++ main.rb
@@ -12,4 +12,32 @@ else
 end
  
 end
-  
+  ----------------------------------------------------
+
+   fibo = "{"
+
+def prime_fib (number)
+    nub1 = 1
+    until nub1 > number
+    
+    ff = "f" << nub1
+    
+    fibonacci = Hash.new{ |h,k| h[k] = k < 2 ? k : h[k-1] + h[k-2] }
+    fiboanwer = fibonacci[nub1]
+    
+     fibo << "p#{nub1} -> #{fiboanwer} , "
+    
+    puts nub1
+    nub1 = nub1 + 1
+end
+    
+    
+
+    return fibo  
+    
+    
+end
+
+puts prime_fib (50)
+
+
